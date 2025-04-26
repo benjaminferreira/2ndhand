9b459fe6bc88ea5aad40ae581291dc89d98b1b88
benjaminferreira <benjamin.j.ferreira@gmail.com>
2025-04-26 18:41:18 -0500

    Added base styles/tailwind setup and some generic test elements to test

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "./2ndhand-logo.png";
+
+export default function Navbar() {
+	return (
+		<nav>
+			<Image
+				src={Logo}
+				alt="2ndhand Logo"
+				width={200}
+				quality={100}
+				placeholder="blur"
+			/>
+			<Link href="/">Dashboard</Link>
+			<Link href="/tickets">Tickets</Link>
+		</nav>
+	);
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,39 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
 		<main>
 			<h2>Dashboard</h2>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla saepe tenetur excepturi alias, deleniti
+				facere necessitatibus veritatis officia, iusto minima, optio autem cumque eum nostrum consequatur enim
+				quisquam voluptatibus? Modi.
+			</p>
+
+			<div className="flex justify-center my-8">
+				<Link href="/tickets">
+					<button className="btn-primary">View Tickets</button>
+				</Link>
+			</div>
+
+			<h2>Company Updates</h2>
+
+			<div className="card">
+				<h3>New member of the web dev team...</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti eaque debitis dolorem. Sit quasi
+					voluptates itaque veritatis eligendi necessitatibus nulla quibusdam id porro voluptas facilis
+					nesciunt, fuga dolorem dolore ut.
+				</p>
+			</div>
+			<div className="card">
+				<h3>New website live!</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, et pariatur. Officia, dolore a
+					architecto repudiandae quaerat odit nemo eius id vero, molestiae earum saepe atque quas hic
+					praesentium explicabo?
+				</p>
+			</div>
 		</main>
 	);
 }
